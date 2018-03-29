5ecf449d034e259b437e3e07d076c2201cc53694
syedkasimm <37894376+syedkasimm@users.noreply.github.com>
2018-03-29 07:33:11 +0000

    Add files via upload

---
+++ student.php
@@ -0,0 +1,23 @@
+<?php
+$m1=50;
+$m2=90;
+$m3=70;
+$S1T= $m1+$m2+$m3;
+$S1A= $m1+$m2+$m3/3;
+$p1=80;
+$p2=90;
+$p3=70;
+$S2T= $p1+$p2+$p3;
+$S2A= $p1+$p2+$p3/3;
+$l1=50;
+$l2=100;
+$l3=70;
+$S3T= $l1+$l2+$l3;
+$S3A= $l1+$l2+$l3/3;
+echo "tot1:",$S1T;
+echo "avg1:",$S1A;
+echo "tot2:",$S2T;
+echo "avg2:",$S2A;
+echo "tot3:",$S3T;
+echo "avg3:",$S3A;
+?>
